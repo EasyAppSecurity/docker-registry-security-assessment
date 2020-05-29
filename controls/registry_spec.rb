8b1fc8e35ee2cdde7e7f79633629072768ecf758
rusakovichma <Mikhail_Rusakovich@epam.com>
2020-05-29 20:43:05 +0300

    image blob check fix

--- controls/registry_spec.rb
+++ controls/registry_spec.rb
@@ -89,13 +89,36 @@ control 'registry-control-04' do
   title 'Verify images blobs download'
   desc 'Verify images blobs download'
   
-  catalog_uri = URI(REGISTRY_SCHEMA + "://" + REGISTRY_HOST + ":" + REGISTRY_PORT + "/" + API_VERSION + "/_catalog")
-  response = Net::HTTP.get(catalog_uri)
+  registry_base = REGISTRY_SCHEMA + "://" + REGISTRY_HOST + ":" + REGISTRY_PORT + "/" + API_VERSION
   
+  catalog_uri = URI(registry_base + "/_catalog")
+  catalog_response = Net::HTTP.get(catalog_uri)
   
-  
-  describe command("echo " + JSON.parse(response)["repositories"][0]) do
-	its(:stdout){should cmp 'sfsdafasdfdsaf' }
+  repositories = JSON.parse(catalog_response)["repositories"]
+  repositories.each do |repository|
+	
+	repository_tags_list_uri = URI(registry_base + "/" + repository + "/tags/list")
+	tags_response = Net::HTTP.get(repository_tags_list_uri)
+	
+	tags = JSON.parse(tags_response)["tags"]
+	tags.each do |tag|
+		
+		repository_tag_manifest = URI(registry_base + "/" + repository + "/manifests/" + tag)
+		manifests_response = Net::HTTP.get(repository_tag_manifest)
+		
+		fsLayers = manifests_response["fsLayers"]
+		fsLayers.each do |fsLayer|
+			image_blob = fsLayer['blobSum'].split(":")[1]
+			repository_blob_url = registry_base + "/" + repository + "/blobs/sha256:" + image_blob
+			
+			describe http(repository_blob_url, ssl_verify: false) do
+				its("status") { should_not cmp 200 }
+			end
+			
+		end
+		
+	end
+	
   end
- 
+   
 end
